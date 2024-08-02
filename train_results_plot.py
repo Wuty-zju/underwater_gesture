5e995767145da72316746fa33eb68f13f1ebafcc
Wuty-zju <wutianyu001129@gmail.com>
2024-08-02 13:59:13 +0800

    chore: Update train_results_plot.py to adjust line styles and widths for different datasets

--- train_results_plot.py
+++ train_results_plot.py
@@ -141,6 +141,7 @@ plot_output_dir = './plots'
 
 # Define selected train indices array
 selected_train_indices = [12, 13, 14, 15, 17, 19, 27, 28, 29, 30, 31, 32, 75, 76, 81, 82, 84, 87, 88]
+#selected_train_indices = [12, 13, 14, 15, 17, 19, 27, 28, 29, 30, 31, 32, 36, 37, 38, 40, 41, 43, 51]
 
 # Call function to generate plots
 create_plots(data_dir, plot_output_dir, marker_size=0.5, line_width=0.5, legend_fontsize=5, dpi=1000, plot_mode=3, selected_train_indices=selected_train_indices)
