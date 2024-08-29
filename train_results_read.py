c77fdf60d046d9fb2d825ec8da3d829816ec88a7
Wuty-zju <wutianyu001129@gmail.com>
2024-08-29 20:24:46 +0800

    Update YOLO model in train.py to use YOLOv9t.yaml

--- train_results_read.py
+++ train_results_read.py
@@ -9,7 +9,7 @@ def get_model_batch_data_from_yaml(yaml_path):
     if os.path.exists(yaml_path):
         with open(yaml_path, 'r') as file:
             data = yaml.safe_load(file)
-        model = data.get('model', '未找到model标签').split('.')[0]
+        model = data.get('model', '未找到model标签')
         batch = data.get('batch', '未找到batch标签')
         data_value = data.get('data', '未找到data标签')
         return model, batch, data_value
@@ -36,7 +36,7 @@ def get_final_epoch(results_csv_path):
 
 def write_csv_and_print_results(base_directory, csv_path):
     """遍历目录并将信息写入CSV文件"""
-    header = ['Train Index', 'Model', 'Batch', 'Epoch', 'Data', 'Path to last.pt', 'Path to best.pt', 'Path to args.yaml', 'Path to results.csv', 'Path to *.0']
+    header = ['Train Index', 'Model', 'Batch', 'Epoch', 'Data', 'Path to last.pt', 'Path to best.pt', 'Path to args.yaml', 'Path to results.csv']
     rows = [header]
 
     pattern = re.compile(r'^train(\d*)$')
@@ -53,12 +53,12 @@ def write_csv_and_print_results(base_directory, csv_path):
             epoch = get_final_epoch(results_csv_path)
             last_pt_path = get_file_path(base_directory, train_dir, 'weights/last.pt')
             best_pt_path = get_file_path(base_directory, train_dir, 'weights/best.pt')
-            event_file = next((get_file_path(base_directory, train_dir, s) for s in os.listdir(full_train_dir) if s.endswith('.0')), 'NaN')
+            #event_file = next((get_file_path(base_directory, train_dir, s) for s in os.listdir(full_train_dir) if s.endswith('.0')), 'NaN')
             
             # 提取 data 路径中的文件名
             data_file_name = os.path.basename(data_value).replace('\\', '/') if data_value != 'NaN' else 'NaN'
 
-            rows.append([train_dir.strip('train') if train_dir.strip('train') != '' else '1', model, batch, epoch, data_file_name, last_pt_path, best_pt_path, args_yaml_path, results_csv_path, event_file])
+            rows.append([train_dir.strip('train') if train_dir.strip('train') != '' else '1', model, batch, epoch, data_file_name, last_pt_path, best_pt_path, args_yaml_path, results_csv_path])
 
     # 写入CSV文件
     with open(csv_path, 'w', newline='', encoding='utf-8') as file:
