e4a4dd1440774053df209d9ebfca3a542749fa19
WuTianyu <wutianyu20001129@gmail.com>
2024-09-03 17:20:47 +0800

    Update YOLO model configuration files to use YOLOv8n.yaml and YOLOv8s.yaml

--- tune.py
+++ tune.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8n.yaml") # YOLOv8n/s/m/l/x or YOLOv9t/s/m/c/e YOLOv10n/s/m/b/l/x
 
-model.tune(data="datasets/CADDY_gestures_YOLO/CADDY_gestures.yaml", epochs=30, iterations=300, optimizer="AdamW", plots=False, save=False, val=False)
+model.tune(data="datasets/CADDY_gestures_YOLO/CADDY_gestures.yaml", epochs=50, iterations=300, optimizer="AdamW")
 
 '''
 Start-Process -FilePath "C:/Users/wutia/Anaconda3/envs/uw_g/python.exe" -ArgumentList "C:/Users/wutia/Desktop/underwater_gesture/tune.py" -RedirectStandardOutput "C:/Users/wutia/Desktop/underwater_gesture/log/tune_1.log" -RedirectStandardError "C:/Users/wutia/Desktop/underwater_gesture/log/tune_2.log"
