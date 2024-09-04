130827849a9ecdec3c53ef07a3dbecff78a70215
Wuty-zju <wutianyu001129@gmail.com>
2024-09-04 21:19:01 +0800

    chore: Update YOLO model in tune.py to use YOLOv10n.yaml

--- train.py
+++ train.py
@@ -2,29 +2,31 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
 
-    # Build a YOLO model from scratch
     model = YOLO('yolov8n.yaml') # YOLOv8n/s/m/l/x or YOLOv9t/s/m/c/e YOLOv10n/s/m/b/l/x
+    #model = YOLO('yolov9s.pt')
 
-    # Build a YOLOv9c model from pretrained weight
-    #model = YOLO('yolov9s.pt') # YOLOv8n/s/m/l or YOLOv9c
-
-    # Display model information (optional)
     model.info()
 
-    # Train the model on the COCO8 example dataset for 100 epochs
-    results = model.train(data='datasets/CADDY_gestures_complete_YOLO/CADDY_gestures_complete.yaml', epochs=100, batch=4, patience=0, pretrained=False)
+    results = model.train(data='datasets/CADDY_gestures_complete_YOLO/CADDY_gestures_complete.yaml', 
+                          epochs=100, 
+                          batch=4, 
+                          patience=0, 
+                          pretrained=False)
+
 '''
-results = model.train(data='datasets/CADDY_gestures_YOLO/CADDY_gestures.yaml', epochs=100, batch=4, patience=0)
-
-Start-Job -ScriptBlock {&'C:/Users/wutia/Anaconda3/envs/uw_g/python.exe' train.py > train.log 2>&1}
-Start-Job -ScriptBlock {& 'C:/Users/wutia/Anaconda3/envs/uw_g/python.exe' 'C:/Users/wutia/Desktop/underwater_gesture/train.py' > 'C:/Users/wutia/Desktop/underwater_gesture/train.log' 2>&1}
+# wty_lab_pc
+Start-Process -FilePath python.exe -ArgumentList train.py -RedirectStandardOutput log/train_1.log -RedirectStandardError log/train_2.log
 Start-Process -FilePath "C:/Users/wutia/Anaconda3/envs/uw_g/python.exe" -ArgumentList "C:/Users/wutia/Desktop/underwater_gesture/train.py" -RedirectStandardOutput "C:/Users/wutia/Desktop/underwater_gesture/log/train_1.log" -RedirectStandardError "C:/Users/wutia/Desktop/underwater_gesture/log/train_2.log"
+# windows
 Start-Process -FilePath "C:/Users/wutia/Anaconda3/envs/uw_g/python.exe" -ArgumentList "C:/Users/wutia/Downloads/underwater_gesture/train.py" -RedirectStandardOutput "C:/Users/wutia/Downloads/underwater_gesture/log/train_1.log" -RedirectStandardError "C:/Users/wutia/Downloads/underwater_gesture/log/train_2.log"
-$process1 = Start-Process -FilePath "C:/Users/wutia/Anaconda3/envs/uw_g/python.exe" -ArgumentList "C:/Users/wutia/Desktop/underwater_gesture/train.py" -RedirectStandardOutput "C:/Users/wutia/Desktop/underwater_gesture/log/train_1.log" -RedirectStandardError "C:/Users/wutia/Desktop/underwater_gesture/log/train_2.log" -PassThru; $process1 | Wait-Process; Start-Process -FilePath "C:/Users/wutia/Anaconda3/envs/uw_g/python.exe" -ArgumentList "C:/Users/wutia/Desktop/underwater_gesture/train_2.py" -RedirectStandardOutput "C:/Users/wutia/Desktop/underwater_gesture/log/train_3.log" -RedirectStandardError "C:/Users/wutia/Desktop/underwater_gesture/log/train_4.log"
 
+# 读取和打印日志
 Get-Content -Path "C:/Users/wutia/Desktop/underwater_gesture/log/train_2.log" -Wait
+
+# 查找和停止进程
 Get-Process -Name "python"
 Get-Process -Name "python" | Stop-Process
 
-while ($true) { $cpu = Get-WmiObject -Class Win32_Processor | Measure-Object -Property LoadPercentage -Average | Select-Object -ExpandProperty Average; $totalMemory = (Get-WmiObject -Class Win32_ComputerSystem).TotalPhysicalMemory; $freeMemory = (Get-WmiObject -Class Win32_OperatingSystem).FreePhysicalMemory * 1KB; $usedMemory = $totalMemory - $freeMemory; $memoryUsagePercent = [math]::round(($usedMemory / $totalMemory) * 100, 2); $gpuInfo = & nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.used --format=csv,noheader,nounits; $gpuUtil = $gpuInfo.Split(",")[0].Trim(); $gpuMemUtil = $gpuInfo.Split(",")[1].Trim(); $gpuMemTotal = $gpuInfo.Split(",")[2].Trim(); $gpuMemUsed = $gpuInfo.Split(",")[3].Trim(); Clear-Host; Write-Host "CPU Usage: $cpu%"; Write-Host "Memory Usage: $([math]::round($usedMemory / 1MB, 2)) MB ($memoryUsagePercent%)"; Write-Host "GPU Usage: $gpuUtil%"; Write-Host "GPU Memory Usage: $gpuMemUsed MB / $gpuMemTotal MB ($gpuMemUtil%)"; Start-Sleep -Seconds 1 }
+# 性能监控
+while ($true) { $cpu = [math]::round((Get-WmiObject Win32_Processor | Measure-Object LoadPercentage -Average).Average, 2); $totalMemory = (Get-WmiObject Win32_ComputerSystem).TotalPhysicalMemory; $freeMemory = (Get-WmiObject Win32_OperatingSystem).FreePhysicalMemory * 1KB; $usedMemory = $totalMemory - $freeMemory; $memoryUsagePercent = [math]::round($usedMemory / $totalMemory * 100, 2); $gpuUtil, $gpuMemUtil, $gpuMemTotal, $gpuMemUsed = (& nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.used --format=csv,noheader,nounits).Split(",").Trim(); $gpuUtil = [math]::round([double]$gpuUtil, 2); $gpuMemUsagePercent = [math]::round($gpuMemUsed / $gpuMemTotal * 100, 2); Clear-Host; Write-Host "CPU Usage: $cpu%"; Write-Host "Memory Usage: $([math]::round($usedMemory / 1MB, 2)) MB / $([math]::round($totalMemory / 1MB, 2)) MB ($memoryUsagePercent%)"; Write-Host "GPU Usage: $gpuUtil%"; Write-Host "GPU Memory Usage: $gpuMemUsed MB / $gpuMemTotal MB ($gpuMemUsagePercent%)"; Start-Sleep 1 }
 '''
--- tune.py
+++ tune.py
@@ -1,13 +1,31 @@
 from ultralytics import YOLO
+from datetime import datetime
 
-model = YOLO("yolov10n.yaml") # YOLOv8n/s/m/l/x or YOLOv9t/s/m/c/e YOLOv10n/s/m/b/l/x
+if __name__ == '__main__':
 
-result_grid = model.tune(data="datasets/CADDY_gestures_YOLO/CADDY_gestures.yaml", epochs=50, iterations=300, use_ray=True)
-model.tune(data="coco8.yaml")
+    #wandb.login(key="e10766eb91aad0e2c30a2ecb8020f5be6eff9211")
+
+    model = YOLO("yolov10n.yaml") # YOLOv8n/s/m/l/x or YOLOv9t/s/m/c/e YOLOv10n/s/m/b/l/x
+
+    current_date = datetime.now().strftime("%Y%m%d")
+
+    trial_dirname = f"{model}_tune_{current_date[:8]}"  # 只使用前8位的日期
+
+    result_grid = model.tune(
+        data="datasets/CADDY_gestures_YOLO/CADDY_gestures.yaml",
+        epochs=50,
+        iterations=300,
+        use_ray=True,
+        trial_dirname_creator=trial_dirname
+    )
 '''
-Start-Process -FilePath "C:/Users/wutia/Anaconda3/envs/uw_g/python.exe" -ArgumentList "C:/Users/wutia/Desktop/underwater_gesture/tune.py" -RedirectStandardOutput "C:/Users/wutia/Desktop/underwater_gesture/log/tune_1.log" -RedirectStandardError "C:/Users/wutia/Desktop/underwater_gesture/log/tune_2.log"
-Start-Process -FilePath "C:/Users/wutia/Anaconda3/envs/uw_g/python.exe" -ArgumentList "C:/Users/wutia/Downloads/underwater_gesture/tune.py" -RedirectStandardOutput "C:/Users/wutia/Downloads/underwater_gesture/log/tune_1.log" -RedirectStandardError "C:/Users/wutia/Downloads/underwater_gesture/log/tune_2.log"
+# 挂起进程
+Start-Process -FilePath python.exe -ArgumentList tune.py -RedirectStandardOutput log/tune_1.log -RedirectStandardError log/tune_2.log
 
+# 查找和停止进程
 Get-Process -Name "python"
 Get-Process -Name "python" | Stop-Process
+
+# 性能监控
+while ($true) { $cpu = [math]::round((Get-WmiObject Win32_Processor | Measure-Object LoadPercentage -Average).Average, 2); $totalMemory = (Get-WmiObject Win32_ComputerSystem).TotalPhysicalMemory; $freeMemory = (Get-WmiObject Win32_OperatingSystem).FreePhysicalMemory * 1KB; $usedMemory = $totalMemory - $freeMemory; $memoryUsagePercent = [math]::round($usedMemory / $totalMemory * 100, 2); $gpuUtil, $gpuMemUtil, $gpuMemTotal, $gpuMemUsed = (& nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.used --format=csv,noheader,nounits).Split(",").Trim(); $gpuUtil = [math]::round([double]$gpuUtil, 2); $gpuMemUsagePercent = [math]::round($gpuMemUsed / $gpuMemTotal * 100, 2); Clear-Host; Write-Host "CPU Usage: $cpu%"; Write-Host "Memory Usage: $([math]::round($usedMemory / 1MB, 2)) MB / $([math]::round($totalMemory / 1MB, 2)) MB ($memoryUsagePercent%)"; Write-Host "GPU Usage: $gpuUtil%"; Write-Host "GPU Memory Usage: $gpuMemUsed MB / $gpuMemTotal MB ($gpuMemUsagePercent%)"; Start-Sleep 1 }
 '''
