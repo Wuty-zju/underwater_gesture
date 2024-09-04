9953f63753b9737e7f3f3550b9549b5b93680083
Wuty-zju <wutianyu001129@gmail.com>
2024-09-04 21:27:41 +0800

    Update YOLO model in tune.py to use YOLOv10n.yaml

--- tune.py
+++ tune.py
@@ -1,23 +1,16 @@
 from ultralytics import YOLO
-from datetime import datetime
 
 if __name__ == '__main__':
 
-    #wandb.login(key="e10766eb91aad0e2c30a2ecb8020f5be6eff9211")
-
     model = YOLO("yolov10n.yaml") # YOLOv8n/s/m/l/x or YOLOv9t/s/m/c/e YOLOv10n/s/m/b/l/x
-
-    current_date = datetime.now().strftime("%Y%m%d")
-
-    trial_dirname = f"{model}_tune_{current_date[:8]}"  # 只使用前8位的日期
 
     result_grid = model.tune(
         data="datasets/CADDY_gestures_YOLO/CADDY_gestures.yaml",
         epochs=50,
         iterations=300,
-        use_ray=True,
-        trial_dirname_creator=trial_dirname
-    )
+        use_ray=True
+        )
+
 '''
 # 挂起进程
 Start-Process -FilePath python.exe -ArgumentList tune.py -RedirectStandardOutput log/tune_1.log -RedirectStandardError log/tune_2.log
