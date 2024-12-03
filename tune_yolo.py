632a974ad0b3ef95f2dd633a35592e8ca494429a
Wu Tianyu <wuty@zju.edu.cn>
2024-12-03 20:00:30 +0800

    更新 requirements.txt 和 tune_yolo.py，调整依赖项版本并修改模型配置和训练参数

--- tune_yolo.py
+++ tune_yolo.py
@@ -5,10 +5,11 @@ if __name__ == '__main__':
     model = YOLO("yolo11x.yaml") # YOLOv8n/s/m/l/x or YOLOv9t/s/m/c/e YOLOv10n/s/m/b/l/x
 
     model.tune(
-        data="datasets/CADDY_gestures_YOLO/CADDY_gestures.yaml",
+        data="datasets/CADDY_gestures_complete_YOLO/CADDY_gestures_complete.yaml",
         batch=8,
-        epochs=50,
-        iterations=300
+        epochs=30,
+        iterations=50,
+        device=[1],
         )
 
 '''
