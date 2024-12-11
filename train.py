245946fde5800fe71d13a3b40927ce88013b33e1
Wu Tianyu <wuty@zju.edu.cn>
2024-12-11 19:01:07 +0800

    更新 train.py，调整 YOLO11 模型配置注释以便于调试和管理

--- train.py
+++ train.py
@@ -28,20 +28,19 @@ model_configs = [
     #"yolo11-C3k2-RAB.yaml",           # sample跑完 nan（关闭amp，x模型val出错）
     #"yolo11-C3k2-HDRAB.yaml", # 尝试amp-nan（nan,关闭amp，x模型val出错）
     #"yolo11x-C2DPB.yaml", "yolo11l-C2DPB.yaml", "yolo11m-C2DPB.yaml", "yolo11s-C2DPB.yaml", "yolo11n-C2DPB.yaml", #RuntimeError: The size of tensor a (336) must match the size of tensor b (400) at non-singleton dimension 3
+    #"yolo11x-C3k2-SFA.yaml", "yolo11l-C3k2-SFA.yaml", "yolo11m-C3k2-SFA.yaml", "yolo11s-C3k2-SFA.yaml", "yolo11n-C3k2-SFA.yaml", nan
 
-    
-    #"yolo11n-C3k2-EMSC.yaml", "yolo11s-C3k2-EMSC.yaml", "yolo11x-C3k2-EMSC.yaml", "yolo11l-C3k2-EMSC.yaml", "yolo11m-C3k2-EMSC.yaml",
+    #"yolo11n-C3k2-EMSC.yaml", "yolo11s-C3k2-EMSC.yaml", "yolo11m-C3k2-EMSC.yaml", "yolo11l-C3k2-EMSC.yaml", "yolo11x-C3k2-EMSC.yaml",
     #"yolo11n-C3k2-EMSCP.yaml", "yolo11s-C3k2-EMSCP.yaml", "yolo11m-C3k2-EMSCP.yaml", "yolo11l-C3k2-EMSCP.yaml", "yolo11x-C3k2-EMSCP.yaml",
-    
-    #"yolo11x-C3k2-SFA.yaml", "yolo11l-C3k2-SFA.yaml", "yolo11m-C3k2-SFA.yaml", "yolo11s-C3k2-SFA.yaml", "yolo11n-C3k2-SFA.yaml", 
     #"yolo11x-C3k2-CTA.yaml", "yolo11l-C3k2-CTA.yaml", "yolo11m-C3k2-CTA.yaml", "yolo11s-C3k2-CTA.yaml", "yolo11n-C3k2-CTA.yaml", 
     
+
     #"yolo11-ReCalibrationFPN-P3456.yaml",
 
     
-    #"yolo11-GlobalEdgeInformationTransfer1.yaml", 
-    #"yolo11-GlobalEdgeInformationTransfer2.yaml", 
-    #"yolo11-GlobalEdgeInformationTransfer3.yaml", 
+    #"yolo11x-GlobalEdgeInformationTransfer1.yaml", "yolo11l-GlobalEdgeInformationTransfer1.yaml", "yolo11m-GlobalEdgeInformationTransfer1.yaml", "yolo11s-GlobalEdgeInformationTransfer1.yaml", "yolo11n-GlobalEdgeInformationTransfer1.yaml", 
+    #"yolo11x-GlobalEdgeInformationTransfer2.yaml", "yolo11l-GlobalEdgeInformationTransfer2.yaml", "yolo11m-GlobalEdgeInformationTransfer2.yaml", "yolo11s-GlobalEdgeInformationTransfer2.yaml", "yolo11n-GlobalEdgeInformationTransfer2.yaml", 
+    #"yolo11x-GlobalEdgeInformationTransfer3.yaml", "yolo11l-GlobalEdgeInformationTransfer3.yaml", "yolo11m-GlobalEdgeInformationTransfer3.yaml", "yolo11s-GlobalEdgeInformationTransfer3.yaml", "yolo11n-GlobalEdgeInformationTransfer3.yaml", 
     
     #"yolo11-C3k2-IDWC.yaml", 
     #"yolo11-C3k2-IDWD.yaml", 
