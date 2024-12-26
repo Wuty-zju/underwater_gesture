a6985bbe6ddd74e4394bd982ed48c01b0c07aece
Wu Tianyu <wuty@zju.edu.cn>
2024-12-26 17:40:27 +0800

    新增 model_detail.py，添加 YOLO 模型加载和错误处理功能

---
+++ model_detail.py
@@ -0,0 +1,20 @@
+import warnings
+warnings.filterwarnings('ignore')
+import os, tqdm
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    yaml_path = "yolo11n-C3k2-WTConv.yaml"
+    
+    error_result = []
+    if 'rtdetr' not in yaml_path and 'cls' not in yaml_path and 'world' not in yaml_path:
+        try:
+            model = YOLO(yaml_path)
+            model.info(detailed=True)
+            model.profile([640, 640])
+            model.fuse()
+        except Exception as e:
+            error_result.append(f'{yaml_path} {e}')
+    
+    for i in error_result:
+        print(i)
